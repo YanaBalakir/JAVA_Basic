4bb34461314751da43168fb26f4790a31a5f44b6
YanaBalakir <32711442+YanaBalakir@users.noreply.github.com>
2017-10-30 02:31:42 +0300

    Update less01_task2.java

--- less01_task2.java
+++ less01_task2.java
@@ -2,6 +2,8 @@ package com.epam.jb.less01;
 
 public class less01_task2 {
 public static void main (String [] args) {
-	System.out.println ("Balakir Yana, Rokossovskogo 58, 375292982805");
+	System.out.println ("Balakir Yana");
+	System.out.println ("pr. Rokossovskogo 58 - 423 ");
+	System.out.println ("+375292982805");
 }
 }
