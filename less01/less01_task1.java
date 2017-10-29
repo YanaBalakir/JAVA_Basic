e4f2f11874284870d5dbcb38ec86d79d4d1463d4
YanaBalakir <32711442+YanaBalakir@users.noreply.github.com>
2017-10-30 02:43:40 +0300

    Add files via upload

---
+++ less01/less01_task1.java
@@ -0,0 +1,7 @@
+package com.epam.jb.less01;
+
+public class less01_task1 {
+public static void main (String [] args) {
+	System.out.println ("Hello,world!");
+}
+}
---
+++ less01/less01_task2.java
@@ -0,0 +1,7 @@
+package com.epam.jb.less01;
+
+public class less01_task2 {
+public static void main (String [] args) {
+	System.out.println ("Balakir Yana, Rokossovskogo 58, 375292982805");
+}
+}
