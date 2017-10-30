2376212bdc9833d7841e0a73a9341275e9e0be18
YanaBalakir <32711442+YanaBalakir@users.noreply.github.com>
2017-10-30 03:46:19 +0300

    Update less01_task4.java

--- less01/less01_task4.java
+++ less01/less01_task4.java
@@ -2,13 +2,13 @@ package com.epam.jb.less01;
 
 public class less01_task4 {
 public static void main(String[] args) {
-double a, b; // äâà êàòåòà
-double c = 0; // ãèïîòåíóçà
-double s; // ïëîùàäü
+double a, b; // два катета
+double c = 0; // гипотенуза
+double s; // площадь
 a = 7;
 b = 9;
 c = Math.sqrt(a * a + b * b);
 s = 0.5 * a * b;
-System.out.println("Ãèïîòåíóçà = " + c + " ïëîùàäü = " + s);
+System.out.println("гипотенуза = " + c + " площадь = " + s);
 }
 }
