fb3619601a9a8537a2016e8de19296c12394e431
YanaBalakir <32711442+YanaBalakir@users.noreply.github.com>
2017-11-21 00:05:21 +0300

    Update task_7.java

--- src/task_7.java
+++ src/task_7.java
@@ -3,37 +3,36 @@ import java.util.Scanner;
 public class task_7 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Please enter a");
+		System.out.println("Р’РІРµРґРёС‚Рµ a");
 		int a = scan.nextInt();
 
 		if (a >= 0) {
 			int a1 = a * a;
-			System.out.println("Квадрат положительного числа" + a + " = " + a1);
+			System.out.println("РљРІР°РґСЂР°С‚ РїРѕР»РѕР¶РёС‚РµР»СЊРЅРѕРіРѕ С‡РёСЃР»Р°" + a + " = " + a1);
 		} else {
 			double a2 = Math.pow(a, 4);
-			System.out.println("Четвертая степень отрицательного числа" + a + "=" + a2);
+			System.out.println("Р§РµС‚РІРµСЂС‚Р°СЏ СЃС‚РµРїРµРЅСЊ РѕС‚СЂРёС†Р°С‚РµР»СЊРЅРѕРіРѕ С‡РёСЃР»Р°" + a + "=" + a2);
 		}
 
-		System.out.println("Please enter b");
+		System.out.println("Р’РІРµРґРёС‚Рµ b");
 		int b = scan.nextInt();
 
 		if (b >= 0) {
 			int b1 = b * b;
-			System.out.println("Квадрат положительного числа" + b + " = " + b1);
+			System.out.println("РљРІР°РґСЂР°С‚ РїРѕР»РѕР¶РёС‚РµР»СЊРЅРѕРіРѕ С‡РёСЃР»Р°" + b + " = " + b1);
 		} else {
 			double b2 = Math.pow(b, 4);
-			System.out.println("Четвертая степень отрицательного числа" + b + "=" + b2);
+			System.out.println("Р§РµС‚РІРµСЂС‚Р°СЏ СЃС‚РµРїРµРЅСЊ РѕС‚СЂРёС†Р°С‚РµР»СЊРЅРѕРіРѕ С‡РёСЃР»Р°" + b + "=" + b2);
 		}
 
-		System.out.println("Please enter c");
+		System.out.println("Р’РІРµРґРёС‚Рµ c");
 		int c = scan.nextInt();
 
 		if (c >= 0) {
 			int c1 = c * c;
-			System.out.println("Квадрат положительного числа" + c + " = " + c1);
+			System.out.println("РљРІР°РґСЂР°С‚ РїРѕР»РѕР¶РёС‚РµР»СЊРЅРѕРіРѕ С‡РёСЃР»Р°" + c + " = " + c1);
 		} else {
-			double c2 = Math.pow(c, 4);
-			System.out.println("Четвертая степень отрицательного числа" + c + "=" + c2);
+			double c2 = Math.pow(c, 4);"Р§РµС‚РІРµСЂС‚Р°СЏ СЃС‚РµРїРµРЅСЊ РѕС‚СЂРёС†Р°С‚РµР»СЊРЅРѕРіРѕ С‡РёСЃР»Р°" + c + "=" + c2);
 		}
 	}
 
