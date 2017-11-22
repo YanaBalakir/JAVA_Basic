7b02a8cd164eb1e2c4cf7f81509651224a8e9646
YanaBalakir <32711442+YanaBalakir@users.noreply.github.com>
2017-11-23 01:14:25 +0300

    Add files via upload

---
+++ Less2-1/task_2_2.java
@@ -0,0 +1,43 @@
+/*Решите задачу, находящую сумму и произведение двух чисел, код которой приведен
+ниже, с использованием методов. */
+
+package tasks;
+
+import java.util.Scanner;
+
+public class task_2_2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a;
+		int b;
+		System.out.println("Enter a");
+
+		while (!sc.hasNextInt()) {
+			sc.next();
+		}
+
+		a = sc.nextInt();
+
+		System.out.println("Enter b");
+		while (!sc.hasNextInt()) {
+			sc.next();
+		}
+
+		b = sc.nextInt();
+
+	}
+
+	int summa(int a, int b) {
+
+		int x;
+		return x = a + b;
+
+	}
+
+	int proizvedenie(int a, int b) {
+		int y;
+		return y = a * b;
+	}
+
+}
