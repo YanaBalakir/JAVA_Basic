cf16e71e55e7997ba8eb753cee757b40815c4a14
YanaBalakir <32711442+YanaBalakir@users.noreply.github.com>
2018-06-29 11:59:01 +0300

    Delete Square.java

--- Figures/src/Square.java
+++
@@ -1,9 +0,0 @@
-public class Square {
-    private double c;
-    public Square (double c){
- final String type = "Square";
-    }
-    public double countSquareS(double c){
-        return c*c;
-    }
-}
