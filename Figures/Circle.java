6497cd26aae1c9f21eef5e0b248d0e51b49c79d8
Dzianis Kulesh <Dzianis_Kulesh@epam.com>
2018-07-01 18:30:56 +0300

    Added comments.

--- Figures/Circle.java
+++ Figures/Circle.java
@@ -1,6 +1,10 @@
+// TODO: DK: Имя пакета принято начинать с домена компании "com.epam", также обычно пакеты называют маленькими буквами.
+
+
 package Figures;
 
 public class Circle implements Figure {
+    // TODO: Если добавлять пустые строки между именем класса, объявлением переменных, методами код будет визуально лучше воспринимать. (IDEA - Ctrl+Alt+L - сделает форматирование.)
     private double r; //радиус
     public Circle (double r){
         this.r = r;
