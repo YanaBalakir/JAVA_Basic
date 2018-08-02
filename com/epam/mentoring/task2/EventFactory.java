6088949f4a258cd42bd95ab2f39b5d28a1562a11
YanaBalakir <32711442+YanaBalakir@users.noreply.github.com>
2018-08-02 13:50:30 +0300

    Create EventFactory.java

---
+++ com/epam/mentoring/task2/EventFactory.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public static ArrayList<Event> createEvents(int id) {
+        ArrayList<Event> minskEvents = new ArrayList<Event>();
+        EventsPropertiesReader.getEventsProperties();
+        if (){
+            minskEvents.add(new Event());
+
+        return minskEvents;
+    }
